1a9b32e69c086f35b263be842a938247b71add51
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-23 16:42:21 -0400

    fthhd

--- redmineconnect.js
+++ redmineconnect.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
   $("#redminelogin").click(function(){
       
     redmineKey = $("#redminekey");
-    //alert(("whatthefuck key ").append(redmineKey));
+    alert(("whatthefuck key ").append(redmineKey));
     
     $.getJSON(url, function(result){
         $.each(result, function(i, field){
