c712d325c30e7f3d0301e6dc9850640da809ce01
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-28 15:46:59 -0400

    rthth

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Redmine Sync'
+      text: 'Redmine Sync',
+      callback: onBtnClick
     }];
   },
   'card-buttons': function(t, options) {
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Redmine Sync'
+      text: 'Redmine Sync',
+      callback: onBtnClick
     }];
   }
 });
