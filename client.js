d67e4e5a069a36989c409c2968f48f913e169d4b
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-28 15:38:25 -0400

    sfghnxfn

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-TrelloPowerUp.initialize({
+window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
