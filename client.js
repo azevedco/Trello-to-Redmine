38a61e79e777defac0434887cb775f388af16660
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-28 15:20:21 -0400

    sgfbs

--- client.js
+++ client.js
@@ -1,10 +1,16 @@
-alert("I'm found!");
+alert("I'm connected");
 
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options)
+  'board-buttons': function(t, options){
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Estimate Size',
+      text: 'Template'
     }];
   },
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Template'
+    }];
+  }
 });
