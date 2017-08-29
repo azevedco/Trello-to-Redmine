d2c45939a7eb3aefe99cd4e2114b82df76fbc35e
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-29 09:34:44 -0400

    ghgndgntg

--- client.js
+++ client.js
@@ -1,13 +1,11 @@
-var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
-};
-
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: 'https://www.reddoorshelter.ca/sites/default/files/success-icon.png',
       text: 'Redmine Sync Board',
-      callback: onBtnClick
+      callback: function (t, opts) {
+          console.log('Someone clicked the button');
+        };
     }, {
       // but of course, you could also just kick off to a url if that's your thing
       icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
@@ -20,7 +18,9 @@ window.TrelloPowerUp.initialize({
     return [{
       icon: 'https://www.reddoorshelter.ca/sites/default/files/success-icon.png',
       text: 'Redmine Sync Card',
-      callback: onBtnClick
+      callback: function (t, opts) {
+          console.log('Someone clicked the button');
+        };
     }];
   }
 });
