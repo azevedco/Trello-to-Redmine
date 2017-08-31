23d513fb57f6b4966124b594ec12a479e5c54a9f
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-31 09:52:19 -0400

    dfdegf

--- redmineconnect.js
+++ redmineconnect.js
@@ -1,6 +1,8 @@
 var host = "";
 var api_Key = "";
 
+var projectId = "";
+
 function setAPIKey(key) {
   this.apiKey = key;
 };
@@ -17,18 +19,39 @@ function getHost() {
   return this.host;
 };
 
+var loadIssues = function(projectId) {
+    $.ajax({
+        url: host + "/issues.json?key=" + api_Key + "&project_id=" + projectId,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function (data) {
+            $("#Issues").empty();
+            $.each(data.issues, function (index, issue) {
+                $("#Issues").append("<tr issueId='" + issue.id + "'><td class='project'>" + issue.project.name
+                    + "</td><td class='idCell'>" + issue.id
+                    + "</td><td class='subject'>" + issue.subject
+                    + "</tr>");
+            });
+        },
+        error: function () {
+            alert('Failed!');
+        },
+    });
+};
+
+
 $(document).ready(function() {
     //$.ajax({
     $("#KeyBtn").click(function() {
         api_Key = $("#ApiKeyText").val();
-        alert(api_Key);
+        host = $("#HostText").val();
         $.ajax({
-            url: window.base_url + "http://135.23.35.134:8099/redmine/projects.json?key=" + api_Key,
+            url: host + "/projects.json?key=" + api_Key,
             type: 'GET',
             crossDomain: true,
             dataType: 'jsonp',
             success: function (data) {
-                alert('Success!');
                 $.each(data.projects, function (index, project) {
                     if (project.status === 1) {
                         $("#ProjectSelect").append('<option value="' + project.id + '">' + project.name + '</option>');
@@ -37,8 +60,22 @@ $(document).ready(function() {
                 });
             },
             error: function () {
-                alert('Failed!');
+                alert('Failed to gain connection to Redmine.');
             },
         });
     });
+    
+    $("#ProjectSelect").change(function() {
+        projectId = $("#ProjectSelect option:selected").attr('value');
+        loadIssues(projectId);
+    });
 });
+
+
+
+
+
+
+
+
+
