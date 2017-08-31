aea8a98958b429898c79177d183293c8a38f84d2
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-31 11:08:42 -0400

    dge

--- CreateUpdateIssue.py
+++ CreateUpdateIssue.py
@@ -1,9 +1,10 @@
 @app.route("/commands/create_issue", methods=["POST"])
 def create_issue():
-    redmine = Redmine(app.base_url, key=app.api_key)    
-    data = request.json
-    issue = redmine.issue.new()
-    issue.project_id = data['project_id']
-    issue.subject = data['subject']
-    issue.save()
+#    redmine = Redmine(app.base_url, key=app.api_key)    
+#    data = request.json
+#    issue = redmine.issue.new()
+#    issue.project_id = data['project_id']
+#    issue.subject = data['subject']
+#    issue.save()
+    print "Fuck"
     return "OK"
