06e349bd4e441ccfc559044a751ab3a085964775
Cole Azevedo <cole.azevedo22@gmail.com>
2017-08-31 16:25:57 -0400

    it is now false

--- client.js
+++ client.js
@@ -65,7 +65,7 @@ window.TrelloPowerUp.initialize({
         // a property 'authorized' being true/false
         // you can also return the object synchronously if you know
         // the answer synchronously
-        return new window.TrelloPowerUp.Promise((resolve) => resolve({ authorized: true }));
+        return new window.TrelloPowerUp.Promise((resolve) => resolve({ authorized: false }));
     },
     'show-authorization': function(t, options){
     // return what to do when a user clicks the 'Authorize Account' link
